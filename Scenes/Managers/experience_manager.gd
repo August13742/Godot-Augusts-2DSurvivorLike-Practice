3ab54930fc88f077cebaeb549b99d4ab491cb1cd
August13742 <149452958+August13742@users.noreply.github.com>
2025-03-29 17:07:37 +0900

    level-up system base implemented

--- Scenes/Managers/experience_manager.gd
+++ Scenes/Managers/experience_manager.gd
@@ -1,7 +1,8 @@
+class_name ExperienceManager
 extends Node
 
 signal experience_updated(current_experience:float, target_experience:int)
-
+signal level_up(new_level:int)
 
 var current_experience:float =0
 var current_level:int = 1
@@ -24,6 +25,7 @@ func increment_experience(number:float):
 		target_experience += experience_growth
 		current_experience = 0
 		experience_updated.emit(current_experience,target_experience)
+		level_up.emit(current_level)
 	
 	
 func on_experience_vial_collected(number:float):
