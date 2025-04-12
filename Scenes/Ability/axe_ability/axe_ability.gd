01f1a7f83ae2779b89f29e503cb70557cb9d8ed4
August13742 <149452958+August13742@users.noreply.github.com>
2025-04-12 23:02:22 +0900

    minor code refinement

--- Scenes/Ability/axe_ability/axe_ability.gd
+++ Scenes/Ability/axe_ability/axe_ability.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var hitbox_component:HitboxComponent = $Visuals/HitboxComponent
+@onready var visuals = $Visuals
+
 @export var max_radius:float = 100
 @export var max_scale:float = 2
 @export var min_scale:float = 1
@@ -9,6 +11,7 @@ extends Node2D
 var scale_up_range = max(max_scale - min_scale,0)
 var root_entity:Node2D
 var base_rotation:Vector2 = Vector2.RIGHT
+var self_root_position = Vector2.ZERO
 func _ready():
 	root_entity = get_tree().get_first_node_in_group("player") # change this for more modularity
 	base_rotation = Vector2.RIGHT.rotated(randf_range(0,TAU))
@@ -21,22 +24,18 @@ func _ready():
 	
 	
 func tween_method(rotations:float):
-	
 	"""
 	sprials outwards in clockwise rotation while scaling size up
 	"""
 	if root_entity == null:
 		return
 		
-	var percent:float = rotations / 2 
+	var percent:float = clamp(rotations / 2.0, 0.0, 1.0)
 	var current_radius:float = percent  * max_radius
 	var current_direction:Vector2 = base_rotation.rotated(rotations*TAU) # clockwise rotation
 	
-	var self_root_position = Vector2.ZERO
 	
-
-
 	self_root_position = root_entity.global_position
 		
 	global_position = self_root_position + (current_direction * current_radius)
-	$Visuals.scale = Vector2.ONE * (min_scale + percent * scale_up_range)
+	visuals.scale = Vector2.ONE * (min_scale + percent * scale_up_range)
