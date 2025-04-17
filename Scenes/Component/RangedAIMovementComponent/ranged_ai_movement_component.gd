36a7b0c01d07535e45cf632f2708813a0c51f4a0
August13742 <149452958+August13742@users.noreply.github.com>
2025-04-17 15:15:28 +0900

    fixed ranged tracking bugs

--- Scenes/Component/RangedAIMovementComponent/ranged_ai_movement_component.gd
+++ Scenes/Component/RangedAIMovementComponent/ranged_ai_movement_component.gd
@@ -6,22 +6,31 @@ class_name RangedAiMovementComponent
 @export var distance_tolerance:float = 50
 
 
-var distance_to_keep_vector:Vector2 = Vector2.ONE * distance_to_keep
-var distance_tolerance_vector:Vector2 = Vector2.ONE * distance_tolerance
-
-
 func _ready():
 	if owner.moving == true:
 		if target_entity == null:
 			print("[Debug/Fallback]: {%s} Target not Set, Defaulting Target to Player"%self.name)
 			target_entity = get_tree().get_first_node_in_group("player")
+	
+	distance_to_keep = (Vector2.ONE * distance_to_keep).length()
+	distance_tolerance = (Vector2.ONE * distance_tolerance).length()
 		
 func get_direction_to_target(target:Node2D = target_entity):
 	if target == null: return Vector2.ZERO
 	
 	var distance_error:Vector2 = target.global_position - owner.global_position
-	if distance_error > distance_tolerance_vector || distance_error < distance_to_keep_vector:
+	var distance_error_length = distance_error.length()
+
+	if (distance_error_length > distance_tolerance) && (distance_error_length < distance_to_keep):
 		return Vector2.ZERO
 			
-	if distance_error > distance_to_keep_vector:
+	if distance_error_length > distance_to_keep:
 		return (distance_error).normalized()
+		
+		
+	return Vector2.ZERO
+
+func target_at_right_direction(target:Node2D = target_entity)->bool:
+	if target == null: return true
+	var x_axis_error = abs(target.global_position.x) - abs(owner.global_position.x)
+	return true if x_axis_error >= 0 else false
