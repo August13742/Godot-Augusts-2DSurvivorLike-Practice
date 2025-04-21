486f292679e2ef9e044acde278cad1fd5061b68a
August13742 <149452958+August13742@users.noreply.github.com>
2025-04-21 18:40:37 +0900

    added hover effect on upgrade cards if cursor on top

--- Scenes/UI/ability_upgrade_card.gd
+++ Scenes/UI/ability_upgrade_card.gd
@@ -1,12 +1,15 @@
 extends PanelContainer
 
-@onready var name_label:Label = $VBoxContainer/NameLabel
+@onready var name_label:RichTextLabel = $VBoxContainer/NameLabel
 @onready var description_label:RichTextLabel = $VBoxContainer/MarginContainer/DescriptionLabel
 @onready var texture_rect:TextureRect = $VBoxContainer/TextureRect
 
 signal chosen 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+	mouse_exited.connect(on_mouse_exited)
+	
 	
 func set_ability_upgrade(upgrade:Ability, current_level:int):
 
@@ -20,3 +23,21 @@ func set_ability_upgrade(upgrade:Ability, current_level:int):
 func on_gui_input(event:InputEvent):
 	if event.is_action_pressed("left_click"):
 		chosen.emit()
+
+
+func on_mouse_entered():
+	var tween = create_tween()
+	tween.tween_property(self,"scale", Vector2(1.1,1.1),0.2).set_ease(Tween.EASE_IN)
+
+
+func on_mouse_exited():
+	self.scale = Vector2.ONE
+	
+
+func _process(_delta):
+	'''
+	without this, if you zoom your cursor across everything too fast, mouse_exited won't be called as it failed to respond
+	'''
+	var mouse_pos = get_global_mouse_position()
+	if not get_global_rect().has_point(mouse_pos) and scale != Vector2.ONE:
+		on_mouse_exited()
